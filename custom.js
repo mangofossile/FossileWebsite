1fb3d38235950f8e07cc241904eb5311ab7aed97
Ashley Fossile <ashleymfossile@gmail.com>
2012-12-03 20:18:09 -0800

    Added Files

---
+++ custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){	
+	$('#mainblock').fadeIn(2000);
+});
+
+//~~~~~Slideshow~~~~~~~~
+$(document).ready(function() {
+    $(function(){
+        $('.fadein img:gt(0)').hide();
+        $('div.txt_content p').html($('div.fadein img:first-child'));
+        setInterval(function(){
+ 
+          $('.fadein :first-child').fadeOut()
+             .next('img').fadeIn()
+             .end().appendTo('.fadein');
+          $('div.txt_content p').html($('div.fadein img:first-child'));
+             }, 2000);
+    });
+});
+
